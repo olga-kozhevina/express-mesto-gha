b540aecc9c17fce353ab02fc2b2bcc0684fc6129
Olga Pomytkina <olpoma@yandex.ru>
2023-06-08 12:13:33 +0700

    validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -45,9 +45,9 @@ const signupValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30).required(),
-    about: Joi.string().min(2).max(30).required(),
-    avatar: Joi.string().regex(urlPattern).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(urlPattern),
   }),
 });
 
